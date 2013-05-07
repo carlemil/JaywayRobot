a57cae551c79b3619d57450e977a5f6e37da7b83
erbsman <erbsman@gmail.com>
2013-05-07 23:22:24 +0100

    added test project with empty tc.

---
+++ RobotTestTest/src/se/kjellstrand/robot/test/BasicRobotTest.java
@@ -0,0 +1,19 @@
+package se.kjellstrand.robot.test;
+
+import junit.framework.TestCase;
+
+public class BasicRobotTest extends TestCase {
+
+    public BasicRobotTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
