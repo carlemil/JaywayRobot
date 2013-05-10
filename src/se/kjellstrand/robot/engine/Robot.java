425ddff1ec84404359aec0531b338728dcc408cc
erbsman <erbsman@gmail.com>
2013-05-10 12:39:04 +0100

    doc of Robot.java

--- src/se/kjellstrand/robot/engine/Robot.java
+++ src/se/kjellstrand/robot/engine/Robot.java
@@ -9,8 +9,16 @@ public class Robot {
      */
     private static final String TAG = Robot.class.getCanonicalName();
 
+    /**
+     * The room that the robot is currently located in, or null if the robot is
+     * in dev/null/ :)
+     */
     private Rect2DRoom mRoom;
 
+    /**
+     * The position that the robot is located at, defined as a RobotLocation,
+     * that consists of, x and y-coordinates and a direction.
+     */
     private RobotLocation mRobotLocation = new RobotLocation();
 
     /**
@@ -23,18 +31,39 @@ public class Robot {
      */
     private int mIntructionPointer = 0;
 
+    /**
+     * The char currently used to represent a move forward instruction in the
+     * robots programming language.
+     */
     private final char FORWARD;
 
+    /**
+     * The char currently used to represent a move left instruction in the
+     * robots programming language.
+     */
     private final char LEFT;
 
+    /**
+     * The char currently used to represent a move right instruction in the
+     * robots programming language.
+     */
     private final char RIGHT;
 
+    /**
+     * Default constructor. Sets the programming language to English.
+     */
     public Robot() {
         this(Language.ENGLISH);
     }
 
+    /**
+     * Constructor that allows for initialising with a programming language
+     * other than the default.
+     * 
+     * @param language The programming language that this robot will understand.
+     */
     public Robot(Language language) {
-        Log.d(TAG, "Robot started, languare: " + language.toString());
+        Log.d(TAG, "Robot started, language: " + language.toString());
         FORWARD = Language.getForwardChar(language);
         LEFT = Language.getLeftChar(language);
         RIGHT = Language.getRightChar(language);
@@ -44,10 +73,22 @@ public class Robot {
         return this.mProgram;
     }
 
+    /**
+     * Returns the position of the robot.
+     * 
+     * @return the robots position.
+     */
     public RobotLocation getRobotPosition() {
         return mRobotLocation;
     }
 
+    /**
+     * Returns true if the robot has not reached the end of its program. Returns
+     * false once the end of the program have been reached.
+     * 
+     * @return true if the robot has more instructions in its program, else
+     *         false.
+     */
     public boolean hasMoreMoves() {
         if (mProgram != null && mProgram.length() > mIntructionPointer) {
             return true;
@@ -55,6 +96,13 @@ public class Robot {
         return false;
     }
 
+    /**
+     * Executes the next instruction (if there is one) in the robots program,
+     * returns the location that the robot ends up at after executing the
+     * instruction.
+     * 
+     * @return the robots location.
+     */
     public RobotLocation move() {
         if (hasMoreMoves()) {
             char command = mProgram.charAt(mIntructionPointer++);
@@ -77,6 +125,51 @@ public class Robot {
             return new RobotLocation(mRobotLocation);
         }
         return null;
+    }
+
+    /**
+     * Executes all the instructions in the robots program, returns the final
+     * position of the robot.
+     * 
+     * @return the robots location.
+     */
+    public RobotLocation moveUntilEnd() {
+        RobotLocation res = null;
+        while (hasMoreMoves()) {
+            res = move();
+        }
+        return res;
+    }
+
+    /**
+     * Positions the robot in "room"
+     * 
+     * @param room the room to put the robot in.
+     */
+    public void putInRoom(Rect2DRoom room) {
+        this.mRoom = room;
+        this.mRobotLocation.setPosition(room.getStartPosition());
+        this.mRobotLocation.setDirection(Direction.NORTH);
+        this.mIntructionPointer = 0;
+    }
+
+    /**
+     * Sets a new program to the robot.
+     * 
+     * @param program The program to set.
+     */
+    public void setProgram(String program) {
+        this.mProgram = program;
+        this.mIntructionPointer = 0;
+    }
+
+    /**
+     * Gets the room that the robot is currently in.
+     * 
+     * @return the room the robot is in.
+     */
+    public Rect2DRoom getRoom() {
+        return mRoom;
     }
 
     private void moveForward(RobotLocation coord) {
@@ -115,26 +208,6 @@ public class Robot {
                 Log.w(TAG, "Unknown Direction: " + dir.toString());
                 break;
         }
-    }
-
-    public RobotLocation moveUntilEnd() {
-        RobotLocation res = null;
-        while (hasMoreMoves()) {
-            res = move();
-        }
-        return res;
-    }
-
-    public void putInRoom(Rect2DRoom room) {
-        this.mRoom = room;
-        this.mRobotLocation.setPosition(room.getStartPosition());
-        this.mRobotLocation.setDirection(Direction.NORTH);
-        this.mIntructionPointer = 0;
-    }
-
-    public void setProgram(String program) {
-        this.mProgram = program;
-        this.mIntructionPointer = 0;
     }
 
     private void turnLeft(RobotLocation coord) {
@@ -179,10 +252,6 @@ public class Robot {
                 Log.w(TAG, "Unknown Direction set: " + dir.toString());
                 break;
         }
-    }
-
-    public Rect2DRoom getRoom() {
-        return mRoom;
     }
 
 }
