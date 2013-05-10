cdba3978d141be35c0e6087c97a099aed5f8c907
erbsman <erbsman@gmail.com>
2013-05-10 13:51:06 +0100

    doc of the Room interface.

--- src/se/kjellstrand/robot/engine/Room.java
+++ src/se/kjellstrand/robot/engine/Room.java
@@ -1,7 +1,22 @@
 package se.kjellstrand.robot.engine;
 
+/**
+ * Interface for a room.
+ * 
+ */
 public interface Room {
+    /**
+     * Tests if a position is contained in a room or not, true if the position is inside the room, else false.
+     * 
+     * @param position the position to check.
+     * @return result of the test.
+     */
     boolean contains(android.graphics.Point position);
 
+    /**
+     * Returns the start position of the room.
+     * 
+     * @return the start position.
+     */
     android.graphics.Point getStartPosition();
 }
